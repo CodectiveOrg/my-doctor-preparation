f7b07759e18c117f22bf7b82c0523697bacb8a37
BijanProgrammer <bijaneisapour@gmail.com>
2024-12-11 15:18:26 +0330

    [ADD] ssr example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+type Post = {
+  id: number;
+  title: string;
+};
+
 export default async function Home() {
-  return <h1>سلام، رفیق! قطعاً</h1>;
+  const res = await fetch("https://jsonplaceholder.org/posts", {
+    cache: "no-store",
+  });
+
+  const posts: Post[] = await res.json();
+
+  return (
+    <>
+      <h1>سلام، رفیق! قطعاً</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
