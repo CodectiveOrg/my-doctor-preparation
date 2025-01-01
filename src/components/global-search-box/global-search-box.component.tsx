2e71591cc328bbbdb6b55c2c7cc8344a361e4ba8
BijanEisapour <bijanprogrammer@gmail.com>
2025-01-01 12:25:30 +0330

    [ADD] search box functionality

--- src/components/global-search-box/global-search-box.component.tsx
+++ src/components/global-search-box/global-search-box.component.tsx
@@ -1,4 +1,8 @@
-import { ReactElement } from "react";
+"use client";
+
+import { FormEvent, ReactElement } from "react";
+
+import { useRouter } from "next/navigation";
 
 import MingcuteSearchLine from "@/icons/MingcuteSearchLine";
 import MingcuteLocationLine from "@/icons/MingcuteLocationLine";
@@ -6,22 +10,34 @@ import MingcuteLocationLine from "@/icons/MingcuteLocationLine";
 import styles from "./global-search-box.module.css";
 
 export default function GlobalSearchBoxComponent(): ReactElement {
+  const router = useRouter();
+
+  const formSubmitHandler = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const query = formData.get("query") ?? "";
+
+    router.push(`/search/?query=${query}`);
+  };
+
   return (
-    <div className={styles["global-search-box"]}>
+    <form className={styles["global-search-box"]} onSubmit={formSubmitHandler}>
       <div className={styles.prefix}>
         <MingcuteSearchLine />
       </div>
       <input
+        name="query"
         type="text"
         placeholder="نام بیماری، تخصص، پزشک، بیمارستان و ..."
       />
       <div className={styles.divider}></div>
       <div className={styles.suffix}>
-        <button>
+        <button type="button">
           <MingcuteLocationLine />
           همه شهرها
         </button>
       </div>
-    </div>
+    </form>
   );
 }
