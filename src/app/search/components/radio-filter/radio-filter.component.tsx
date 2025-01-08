182bc51f2a1b5b0b085a6048f53087bcec75b9a2
BijanProgrammer <bijaneisapour@gmail.com>
2025-01-08 18:57:13 +0330

    [ADD] gender filter functionality

--- src/app/search/components/radio-filter/radio-filter.component.tsx
+++ src/app/search/components/radio-filter/radio-filter.component.tsx
@@ -1,4 +1,6 @@
-import { ReactElement } from "react";
+"use client";
+
+import { ChangeEvent, ReactElement } from "react";
 
 import CardComponent from "@/components/card/card.component";
 
@@ -10,20 +12,34 @@ type Props = {
   title: string;
   name: string;
   options: SelectOptionType[];
+  value?: string;
+  onChange?: (value: string) => void;
 };
 
 export default function RadioFilterComponent({
   title,
   name,
   options,
+  value,
+  onChange,
 }: Props): ReactElement {
+  const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+    onChange?.(e.currentTarget.value);
+  };
+
   return (
     <CardComponent>
       <div className={styles["radio-filter"]}>
         <div className={styles.title}>{title}</div>
         {options.map((x) => (
           <label key={x.value}>
-            <input type="radio" name={name} value={x.value} />
+            <input
+              type="radio"
+              name={name}
+              value={x.value}
+              checked={x.value === value}
+              onChange={inputChangeHandler}
+            />
             {x.label}
           </label>
         ))}
