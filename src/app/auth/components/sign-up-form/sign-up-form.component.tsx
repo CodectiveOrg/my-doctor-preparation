dc41dea6e40ec2b41f53c16347397d03d23e919a
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-20 17:22:49 +0330

    [ADD] form submit handler

--- src/app/auth/components/sign-up-form/sign-up-form.component.tsx
+++ src/app/auth/components/sign-up-form/sign-up-form.component.tsx
@@ -1,4 +1,6 @@
-import { ReactElement } from "react";
+"use client";
+
+import { ReactElement, FormEvent } from "react";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -8,40 +10,74 @@ import signUpImage from "@/assets/images/sign-up.webp";
 import { ButtonComponent } from "@/components/button/button.component";
 import CardComponent from "@/components/card/card.component";
 import NormalInputComponent from "@/components/normal-input/normal-input.component";
+import PasswordInputComponent from "@/components/password-input/password-input.component";
+
+import { SignUpDto } from "@/dto/auth.dto";
 
 import MingcuteIncognitoModeLine from "@/icons/MingcuteIncognitoModeLine";
 import MingcuteUser3Line from "@/icons/MingcuteUser3Line";
 import MingcuteMailLine from "@/icons/MingcuteMailLine";
-import MingcuteKey2Line from "@/icons/MingcuteKey2Line";
-import MingcuteEye2Line from "@/icons/MingcuteEye2Line";
 
 import styles from "./sign-up-form.module.css";
-import PasswordInputComponent from "@/components/password-input/password-input.component";
 
 export default function SignUpFormComponent(): ReactElement {
+  const formSubmitHandler = async (
+    e: FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const dto: SignUpDto = {
+      name: formData.get("name") as string,
+      username: formData.get("username") as string,
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    };
+
+    const response = await fetch("/api/auth/sign-up", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dto),
+    });
+
+    console.log("response", { ...response });
+
+    if (!response.ok) {
+      return;
+    }
+
+    const data = await response.json();
+
+    console.log("data", { ...data });
+  };
+
   return (
     <div className={styles["sign-up-form"]}>
       <CardComponent>
         <div className={styles["card-content"]}>
           <div className={styles.writings}>
             <h1>خوش آمدید!</h1>
-            <form>
+            <form onSubmit={formSubmitHandler}>
               <NormalInputComponent
                 label="نام و نام خانوادگی"
                 type="text"
+                name="name"
                 prefixIcon={<MingcuteIncognitoModeLine />}
               />
               <NormalInputComponent
                 label="نام کاربری"
                 type="text"
+                name="username"
                 prefixIcon={<MingcuteUser3Line />}
               />
               <NormalInputComponent
                 label="ایمیل"
                 type="email"
+                name="email"
                 prefixIcon={<MingcuteMailLine />}
               />
-              <PasswordInputComponent label="رمز عبور" />
+              <PasswordInputComponent label="رمز عبور" name="password" />
               <ButtonComponent variant="primary">ثبت‌نام</ButtonComponent>
             </form>
             <div className={styles["change-form"]}>
