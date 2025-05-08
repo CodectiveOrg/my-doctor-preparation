fe205d7875e73e82715dfb50598c8130ca2c6e56
BijanProgrammer <bijaneisapour@gmail.com>
2025-05-08 15:37:27 +0330

    [ADD] dashboard layout

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ReactElement } from "react";
+
+import styles from "./page.module.css";
+
+export default function Page(): ReactElement {
+  return (
+    <div className={styles.page}>
+      <h1>ویرایش پروفایل</h1>
+    </div>
+  );
+}
