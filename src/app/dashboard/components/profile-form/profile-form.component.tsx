4171b5d0f7b2be45f669853fe5e30d06bbb4b0ef
BijanProgrammer <bijaneisapour@gmail.com>
2025-05-10 14:10:03 +0330

    [ADD] edit profile logic

--- src/app/dashboard/components/profile-form/profile-form.component.tsx
+++ src/app/dashboard/components/profile-form/profile-form.component.tsx
@@ -1,4 +1,6 @@
-import { FormEvent, ReactElement, useRef } from "react";
+import { FormEvent, ReactElement, useEffect, useState } from "react";
+
+import Loading from "@/app/loading";
 
 import { ButtonComponent } from "@/components/button/button.component";
 import CardComponent from "@/components/card/card.component";
@@ -16,27 +18,39 @@ import { fetchWithToast } from "@/utils/fetch-utils";
 import styles from "./profile-form.module.css";
 
 export default function ProfileFormComponent(): ReactElement {
-  const formRef = useRef<HTMLFormElement>(null);
+  const [values, setValues] = useState<EditProfileDto>({});
+  const [status, setStatus] = useState<"pending" | "error" | "success">(
+    "pending",
+  );
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const result = await fetchWithToast<EditProfileDto>(
+        "/api/dashboard/profile",
+      );
+
+      if (result.error) {
+        setStatus("error");
+        return;
+      }
+
+      setValues(result.data!);
+      setStatus("success");
+    };
+
+    fetchProfile().then();
+  }, []);
 
   const formSubmitHandler = async (
     e: FormEvent<HTMLFormElement>,
   ): Promise<void> => {
     e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-
-    const dto: EditProfileDto = {
-      name: (formData.get("name") as string | null) ?? undefined,
-      username: (formData.get("username") as string | null) ?? undefined,
-      email: (formData.get("email") as string | null) ?? undefined,
-      password: (formData.get("password") as string | null) ?? undefined,
-    };
-
     const result = await fetchWithToast<null>(
-      "/api/user/profile",
+      "/api/dashboard/profile",
       {
         method: "PATCH",
-        body: JSON.stringify(dto),
+        body: JSON.stringify(values),
       },
       "ویرایش با موفقیت انجام شد.",
     );
@@ -46,32 +60,56 @@ export default function ProfileFormComponent(): ReactElement {
     }
   };
 
+  if (status === "pending") {
+    return <Loading />;
+  }
+
+  if (status === "error") {
+    return <h1>نشد که بشه :(</h1>;
+  }
+
   return (
     <CardComponent className={styles["profile-form"]}>
       <h1>ویرایش پروفایل</h1>
-      <form ref={formRef} onSubmit={formSubmitHandler}>
+      <form onSubmit={formSubmitHandler}>
         <NormalInputComponent
           label="نام و نام خانوادگی"
           type="text"
           name="name"
           prefixIcon={<MingcuteIncognitoModeLine />}
+          value={values.name}
+          onChange={(e) =>
+            setValues((old) => ({ ...old, name: e.target.value }))
+          }
         />
         <NormalInputComponent
           label="نام کاربری"
           type="text"
           name="username"
           prefixIcon={<MingcuteUser3Line />}
+          value={values.username}
+          onChange={(e) =>
+            setValues((old) => ({ ...old, username: e.target.value }))
+          }
         />
         <NormalInputComponent
           label="ایمیل"
           type="email"
           name="email"
           prefixIcon={<MingcuteMailLine />}
+          value={values.email}
+          onChange={(e) =>
+            setValues((old) => ({ ...old, email: e.target.value }))
+          }
         />
         <PasswordInputComponent
           label="رمز عبور"
           name="password"
           autoComplete="new-password"
+          value={values.password}
+          onChange={(e) =>
+            setValues((old) => ({ ...old, password: e.target.value }))
+          }
         />
         <ButtonComponent variant="primary">ذخیره</ButtonComponent>
       </form>
